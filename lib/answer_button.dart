548d4d0fa185fb343b95e7c6dc96d02b8590beb2
Steve Kamau <steve@tellusapp.com>
2024-07-04 07:56:43 +0300

    Added new screen & list

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.buttonText, this.onClick, {super.key});
+  final String buttonText;
+  final Function() onClick;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
+      onPressed: onClick,
+      child: Text(buttonText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/data/questions.dart';
+import 'answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,9 +11,34 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
+void onAnswerClick() {}
+
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions screen');
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffleAnswers().map((item) {
+              return AnswerButton(item, onAnswerClick);
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
